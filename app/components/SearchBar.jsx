7b4a065fb849efaffa8bd5c9d71d4fa772993181
Martin <martinlsalinas@pm.me>
2025-03-16 13:45:29 -0500

    add: SearchBar Component

---
+++ app/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+'use client'
+
+export default function SearchBar () { 
+  return ( 
+    <div className="container">
+    <div className="row">
+    <input type="text" placeholder="Search Text" />
+    </div>
+    <button type="buttn" className="btn btn-primary"> Search </button>
+    </div>
+  )
+}
+
+
